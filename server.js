9496aa9274186aee55477dc462821ed52c1a85e3
rafactor <avila.rafael@gmail.com>
2019-03-09 12:10:38 -0500

    added mongoose

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose')
+const keys = require("./config/keys")
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+// Connect to the Mongo DB
+mongoose.connect(keys.mongodb.dbURI || "mongodb://localhost/reactreadinglist",()=> {
+  console.log('connected to mongo DB')
+})
 
 // Send every request to the React app
 // Define any API routes before this runs
